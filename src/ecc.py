73a165a741a29ceb9dd7dbfa24082d68021d3813
Rico Angell <rangell@umass.edu>
2021-12-25 08:34:33 -0500

    finished trellis cutting code with merge memberships

--- src/ecc.py
+++ src/ecc.py
@@ -201,6 +201,33 @@ class EccClusterer(object):
                 data[j] = i
         return data
 
+    @staticmethod
+    @nb.njit
+    def merge_memberships(lchild_indices: np.ndarray,
+                          lchild_data: np.ndarray,
+                          rchild_indices: np.ndarray,
+                          rchild_data: np.ndarray,
+                          parent_indices: np.ndarray,
+                          parent_data: np.ndarray):
+        assert lchild_indices.size == lchild_data.size
+        assert rchild_indices.size == rchild_data.size
+        assert parent_indices.size == parent_data.size
+        assert (lchild_data.size + rchild_data.size) == parent_data.size
+        lchild_ptr = 0
+        rchild_ptr = 0
+        for i in range(parent_data.size):
+            if ((lchild_ptr < lchild_indices.size and
+                    lchild_indices[lchild_ptr] < rchild_indices[rchild_ptr])
+                  or rchild_ptr == rchild_indices.size):
+                assert parent_indices[i] == lchild_indices[lchild_ptr]
+                parent_data[i] = lchild_data[lchild_ptr]
+                lchild_ptr += 1
+            else:
+                assert parent_indices[i] == rchild_indices[rchild_ptr]
+                assert lchild_indices[lchild_ptr] != rchild_indices[rchild_ptr]
+                parent_data[i] = rchild_data[rchild_ptr]
+                rchild_ptr += 1
+
     def cut_trellis(self, t: Trellis):
         num_ecc = len(self.ecc_constraints)
         num_points = self.n - num_ecc
@@ -213,23 +240,35 @@ class EccClusterer(object):
         num_ecc_sat = np.zeros((t.num_nodes,))
 
         for node in t.internal_nodes_topo_ordered():
-            leaves = membership_indices[membership_indptr[node]:
-                                        membership_indptr[node+1]]
+            node_start = membership_indptr[node]
+            node_end = membership_indptr[node+1]
+            leaves = membership_indices[node_start:node_end]
             if num_ecc > 0:
                 num_ecc_sat[node] = self.get_num_ecc_sat(leaves, num_points)
             obj_vals[node] = self.get_intra_cluster_energy(leaves)
-
             for lchild, rchild in t.get_child_pairs_iter(node):
-                chp_num_ecc_sat = num_ecc_sat[lchild] + num_ecc_sat[rchild]
-                chp_obj_val = obj_vals[lchild] + obj_vals[rchild]
-                if (num_ecc_sat[node] > chp_num_ecc_sat 
-                    or (num_ecc_sat[node] == chp_num_ecc_sat
-                        and obj_vals[node] > chp_obj_val)):
-                    num_ecc_sat[node] = chp_num_ecc_sat
-                    obj_vals[node] = chp_obj_val
-                    # TODO: update membership_data
-                    embed()
-                    exit()
+                cpair_num_ecc_sat = num_ecc_sat[lchild] + num_ecc_sat[rchild]
+                cpair_obj_val = obj_vals[lchild] + obj_vals[rchild]
+                if (num_ecc_sat[node] < cpair_num_ecc_sat 
+                    or (num_ecc_sat[node] == cpair_num_ecc_sat
+                        and obj_vals[node] < cpair_obj_val)):
+                    num_ecc_sat[node] = cpair_num_ecc_sat
+                    obj_vals[node] = cpair_obj_val
+                    lchild_start = membership_indptr[lchild]
+                    lchild_end = membership_indptr[lchild+1]
+                    rchild_start = membership_indptr[rchild]
+                    rchild_end = membership_indptr[rchild+1]
+                    self.merge_memberships(
+                            membership_indices[lchild_start:lchild_end],
+                            membership_data[lchild_start:lchild_end],
+                            membership_indices[rchild_start:rchild_end],
+                            membership_data[rchild_start:rchild_end],
+                            membership_indices[node_start:node_end],
+                            membership_data[node_start:node_end],
+                    )
+
+        embed()
+        exit()
 
         best_clustering = membership_data[-(num_points+num_ecc):-num_ecc]
 
